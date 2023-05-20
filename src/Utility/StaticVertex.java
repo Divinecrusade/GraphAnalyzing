399d01484cbe25995ae79697a9eea9c67ebf534f
DivineCrusade <gorlovdima@inbox.ru>
2023-05-20 13:17:43 +0300

    feat: Add implementation of IVertex interface

---
+++ src/Utility/StaticVertex.java
@@ -0,0 +1,23 @@
+package Utility;
+
+import java.awt.*;
+
+class StaticVertex implements IVertex {
+    public StaticVertex(String name, Point pos) {
+        this.name = name;
+        this.pos  = pos;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Point getPos() {
+        return pos;
+    }
+
+    final private String name;
+    final private Point  pos;
+}
