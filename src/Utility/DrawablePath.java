f63c9b9ae61a7a880aedc9c459c3259c55de59cd
DivineCrusade <gorlovdima@inbox.ru>
2023-05-21 15:47:30 +0300

    fix: Distance label incorrect draw TODO: fix selection of vertexes

--- src/Utility/DrawablePath.java
+++ src/Utility/DrawablePath.java
@@ -17,9 +17,18 @@ public class DrawablePath {
         gfx.drawLine(path.getBegin().getPos().x, path.getBegin().getPos().y,
                      path.getEnd().getPos().x, path.getEnd().getPos().y);
 
-        gfx.drawString(Double.toString(path.getDistance()),
-                     Math.abs(path.getEnd().getPos().x - path.getBegin().getPos().x) / 2,
-                     Math.abs(path.getEnd().getPos().y - path.getBegin().getPos().y) / 2);
+        String label = Double.toString(path.getDistance());
+        int labelWidth = gfx.getFontMetrics().stringWidth(label);
+        int labelHeight = gfx.getFontMetrics().getFont().getSize();
+        Point center = new Point((path.getEnd().getPos().x + path.getBegin().getPos().x) / 2,
+                                 (path.getEnd().getPos().y + path.getBegin().getPos().y) / 2);
+        Rectangle box = new Rectangle(center.x - labelWidth / 2,
+                                      center.y - labelHeight / 2,
+                                      labelWidth, labelHeight + 4);
+        gfx.setColor(Color.WHITE);
+        gfx.fillRect(box.x, box.y, box.width, box.height);
+        gfx.setColor(Color.BLACK);
+        gfx.drawString(label, box.x, box.y + labelHeight);
     }
 
     public boolean isTheSame(IPath other_path) {
