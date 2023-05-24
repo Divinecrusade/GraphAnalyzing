9b571ebea6d16449b4f44dc17c81c02da98eeb95
DivineCrusade <gorlovdima@inbox.ru>
2023-05-24 21:36:37 +0300

    feat: Add panel with inputs and buttons

--- src/Application/Window.java
+++ src/Application/Window.java
@@ -2,6 +2,7 @@ package Application;
 
 import Controller.GraphConstructor;
 import Model.Graph;
+import View.ControlPanel;
 import View.GraphArea;
 
 import javax.swing.*;
@@ -11,16 +12,36 @@ public class Window extends JFrame {
 
     public Window() {
         super("Graph analyzing");
-        setSize(800, 600);
+        setSize(1200, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         graphStruct = new Graph();
         graphArea = new GraphArea(new Font("serif", Font.BOLD, 16), 20, 4);
         graphConstructor = new GraphConstructor(graphStruct, graphArea, graphArea);
+        controlPanel = new ControlPanel(getFontMetrics(new Font("serif", Font.PLAIN, 16)), Color.GRAY);
+        GridBagLayout gbl = new GridBagLayout();
+        setLayout(gbl);
+        GridBagConstraints gbc = new GridBagConstraints();
 
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+
+        gbc.weighty = 1;
+        gbc.weightx = 0.975;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbl.setConstraints(graphArea, gbc);
         this.add(graphArea);
 
+        gbc.weightx = 0.025;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.insets.top = 10;
+
+        gbl.setConstraints(controlPanel, gbc);
+        this.add(controlPanel);
         setVisible(true);
+
+        getContentPane().setBackground(Color.GRAY);
     }
 
     public static void main(String[] args) {
@@ -29,6 +50,7 @@ public class Window extends JFrame {
 
     Graph graphStruct;
     GraphArea graphArea;
+    ControlPanel controlPanel;
     GraphConstructor graphConstructor;
 
 }
--- src/View/ControlPanel.java
+++ src/View/ControlPanel.java
@@ -1,4 +1,99 @@
 package View;
 
-public class ControlPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class ControlPanel extends JPanel {
+    public ControlPanel(FontMetrics fnt, Color bg_colour) {
+        super();
+        super.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        inputVertBeg = new JTextField();
+        inputVertEnd = new JTextField();
+        outputOptimalPath = new JTextField();
+        outputOptimalPathLength = new JTextField();
+        findOptimalPath = new JButton();
+        deleteGraph = new JButton();
+
+        String sample = "Найти оптимальный путь";
+        Dimension minInputSize = new Dimension(fnt.stringWidth(sample), fnt.getFont().getSize() + 2);
+        Dimension prefInputSize = new Dimension(minInputSize);
+        Dimension maxInputSize = new Dimension((int)(minInputSize.width * 1.5), (minInputSize.height));
+
+        inputVertBeg.setMinimumSize(minInputSize);
+        inputVertBeg.setPreferredSize(prefInputSize);
+        inputVertBeg.setMaximumSize(maxInputSize);
+
+        inputVertEnd.setMinimumSize(minInputSize);
+        inputVertEnd.setPreferredSize(prefInputSize);
+        inputVertEnd.setMaximumSize(maxInputSize);
+
+        Dimension minOutputSize = new Dimension((int)(minInputSize.width * 2.5), minInputSize.height);
+        Dimension prefOutputSize = new Dimension(minOutputSize);
+        Dimension maxOutputSize = new Dimension(Short.MAX_VALUE, (minInputSize.height));
+
+        outputOptimalPath.setMinimumSize(minOutputSize);
+        outputOptimalPath.setPreferredSize(prefOutputSize);
+        outputOptimalPath.setMinimumSize(maxOutputSize);
+
+        outputOptimalPathLength.setMinimumSize(minOutputSize);
+        outputOptimalPathLength.setPreferredSize(prefOutputSize);
+        outputOptimalPathLength.setMinimumSize(maxOutputSize);
+
+        inputsGroup = new JPanel();
+        outputsGroup = new JPanel();
+        buttonsGroup = new JPanel();
+
+        inputsGroup.setLayout(new BoxLayout(inputsGroup, BoxLayout.LINE_AXIS));
+        inputsGroup.add(inputVertBeg);
+        inputsGroup.add(Box.createHorizontalGlue());
+        inputsGroup.add(inputVertEnd);
+
+        outputsGroup.setLayout(new BoxLayout(outputsGroup, BoxLayout.PAGE_AXIS));
+        outputsGroup.add(outputOptimalPath);
+        outputsGroup.add(outputOptimalPathLength);
+
+        String buttonFindLabel = "Найти оптимальный путь";
+        String buttonDeleteLabel = "Очистить граф";
+        findOptimalPath.setText(buttonFindLabel);
+        deleteGraph.setText(buttonDeleteLabel);
+        Dimension minButtonFindSize = new Dimension((int)(minOutputSize.width * 0.5), fnt.getFont().getSize() * 2 + 2);
+        Dimension minButtonDeleteSize = new Dimension((int)(minOutputSize.width * 0.4), fnt.getFont().getSize() * 2 + 2);
+        Dimension prefButtonFindSize = new Dimension(minButtonFindSize);
+        Dimension prefButtonDeleteSize = new Dimension(minButtonDeleteSize);
+        Dimension maxButtonFindSize = new Dimension((int)(Short.MAX_VALUE * 0.5), fnt.getFont().getSize() * 2 + 2);
+        Dimension maxButtonDeleteSize = new Dimension((int)(Short.MAX_VALUE * 0.4), fnt.getFont().getSize() * 2 + 2);
+
+        findOptimalPath.setMinimumSize(minButtonFindSize);
+        findOptimalPath.setPreferredSize(prefButtonFindSize);
+        findOptimalPath.setMaximumSize(maxButtonFindSize);
+
+        deleteGraph.setMinimumSize(minButtonDeleteSize);
+        deleteGraph.setPreferredSize(prefButtonDeleteSize);
+        deleteGraph.setMaximumSize(maxButtonDeleteSize);
+
+        buttonsGroup.setLayout(new BoxLayout(buttonsGroup, BoxLayout.LINE_AXIS));
+        buttonsGroup.add(findOptimalPath);
+        buttonsGroup.add(Box.createHorizontalGlue());
+        buttonsGroup.add(deleteGraph);
+
+        add(inputsGroup);
+        add(outputsGroup);
+        add(buttonsGroup);
+
+        inputsGroup.setBackground(bg_colour);
+        outputsGroup.setBackground(bg_colour);
+        buttonsGroup.setBackground(bg_colour);
+    }
+
+    private JTextField inputVertBeg;
+    private JTextField inputVertEnd;
+    private JTextField outputOptimalPath;
+    private JTextField outputOptimalPathLength;
+    private JButton findOptimalPath;
+    private JButton deleteGraph;
+
+    private JPanel inputsGroup;
+    private JPanel outputsGroup;
+    private JPanel buttonsGroup;
 }
